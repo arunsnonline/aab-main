4d69e47aeca5514cf1d995054e45409585d9b081
arunsn85 <hibyevery1@gmail.com>
2013-04-10 20:11:03 +0530

    Added feature to load map and pan to user's current location.Otherwise map  will load to india

--- src/main/webapp/js/editmap.js
+++ src/main/webapp/js/editmap.js
@@ -1,22 +1,59 @@
-$(document).ready(function() {
-	   initialize();	
+$(document).ready(function() {	
 	    var geocoder;
 		var map;
+		var initialLocation;
+		var india = new google.maps.LatLng(22.00, 77.00);
+		var browserSupportFlag =  new Boolean();
+		
+	  initialize();
       function initialize() {
 		geocoder = new google.maps.Geocoder();
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
+          zoomControl: true,
+		  zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.LARGE
+		  },
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+        
+     // Try W3C Geolocation (Preferred)
+        if(navigator.geolocation) {
+          browserSupportFlag = true;
+          navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(initialLocation);
+          }, function() {
+            handleNoGeolocation(browserSupportFlag);
+          });
+        }
+        // Browser doesn't support Geolocation
+        else {
+          browserSupportFlag = false;
+          handleNoGeolocation(browserSupportFlag);
+        }
+
       }
       
 	  
 	  $("#geocode").click(function(){
 		codeAddress();
 	  });
+	  
+	  function handleNoGeolocation(errorFlag) {
+		    if (errorFlag == true) {
+		      alert("Geolocation service failed.");
+		      initialLocation = india;
+		    } else {
+		      alert("Your browser doesn't support geolocation. We've placed you in India.");
+		      initialLocation = india;
+		    }
+		    map.setCenter(initialLocation);
+		}
+		
+		
       
       function codeAddress() {
     	    var address = $("#detailedLocation").val();
